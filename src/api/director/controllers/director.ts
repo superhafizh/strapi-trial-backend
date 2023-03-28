a1e7b877438a4c98f91e0abf4576ff52f298fe5f
Hafizh Ilman Aziz <superhafizh@gmail.com>
2023-03-28 20:58:06 +0700

    initial commit

---
+++ src/api/director/controllers/director.ts
@@ -0,0 +1,7 @@
+/**
+ * director controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::director.director');
---
+++ src/api/director/routes/director.ts
@@ -0,0 +1,7 @@
+/**
+ * director router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::director.director');
